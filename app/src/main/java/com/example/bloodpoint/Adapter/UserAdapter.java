627932e3e2c627b255dd4dec4b224865e0f48acb
Pranav <pranavmodi64@gmail.com>
2022-10-02 11:08:27 +0530

    An Blood Donation App

---
+++ app/src/main/java/com/example/bloodpoint/Adapter/UserAdapter.java
@@ -0,0 +1,5 @@
+package com.example.bloodpoint.Adapter;
+
+public class UserAdapter {
+
+}
---
+++ app/src/main/java/com/example/bloodpoint/MainActivity.java
@@ -0,0 +1,235 @@
+package com.example.bloodpoint;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Notification;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.bloodpoint.Model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView nav_view;
+
+    private CircleImageView nav_profile_image;
+    private TextView nav_fullname, nav_email, nav_bloodgroup, nav_type;
+
+    private DatabaseReference userRef;
+
+    private RecyclerView recyclerView;
+    private ProgressBar progressbar;
+
+    private List<User> userList;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Blood Point");
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        nav_view = findViewById(R.id.nav_view);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        nav_view.setNavigationItemSelectedListener(this);
+
+        progressbar = findViewById(R.id.progressbar);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        userList = new ArrayList<>();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference()
+                .child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String type = snapshot.child("type").getValue().toString();
+                if (type.equals("Donor")) {
+                    readRecipients();
+                } else {
+                    readDonors();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        nav_profile_image = nav_view.getHeaderView(0).findViewById(R.id.nav_user_image);
+        nav_fullname = nav_view.getHeaderView(0).findViewById(R.id.nav_user_fullname);
+        nav_email = nav_view.getHeaderView(0).findViewById(R.id.nav_user_email);
+        nav_bloodgroup = nav_view.getHeaderView(0).findViewById(R.id.nav_user_bloodgroup);
+        nav_type = nav_view.getHeaderView(0).findViewById(R.id.nav_user_type);
+
+
+        userRef = FirebaseDatabase.getInstance().getReference().child("users").child(
+                FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+
+                    String name = snapshot.child("name").getValue().toString();
+                    nav_fullname.setText(name);
+
+                    String email = snapshot.child("email").getValue().toString();
+                    nav_email.setText(email);
+
+                    String bloodgroup = snapshot.child("bloodgroup").getValue().toString();
+                    nav_bloodgroup.setText(bloodgroup);
+
+                    String type = snapshot.child("type").getValue().toString();
+                    nav_type.setText(type);
+
+                    if (snapshot.hasChild("profilepictureurl")) {
+                        String imageUrl = snapshot.child("profilepictureurl").getValue().toString();
+                        Glide.with(getApplicationContext()).load(imageUrl).into(nav_profile_image);
+                    } else {
+                        nav_profile_image.setImageResource(R.drawable.profile_image);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+            }
+        });
+    }
+
+
+    private void readDonors() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                .child("users");
+        Query query = reference.orderByChild("type").equalTo("Donor");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    User user = dataSnapshot.getValue(User.class);
+                    userList.add(user);
+                }
+                progressbar.setVisibility(View.GONE);
+
+                if (userList.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "No Donor Available", Toast.LENGTH_SHORT).show();
+                    progressbar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
+    private void readRecipients() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                .child("users");
+        Query query = reference.orderByChild("type").equalTo("Recipient");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    User user = dataSnapshot.getValue(User.class);
+                    userList.add(user);
+                }
+                progressbar.setVisibility(View.GONE);
+
+                if (userList.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "No Recipient Available", Toast.LENGTH_SHORT).show();
+                    progressbar.setVisibility(View.GONE);
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.profile:
+                Intent intent1 = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent1);
+                finish();
+        }
+
+        switch (item.getItemId()) {
+            case R.id.logout:
+                Intent intent2 = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent2);
+                finish();
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
